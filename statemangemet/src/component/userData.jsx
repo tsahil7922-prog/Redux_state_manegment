5be5066af509f02060d7711eeb47a8538aabc592
tsahil7922-prog <tsahil7922@gmail.com>
2026-09-02 21:14:10 +0000

    done

---
+++ statemangemet/src/component/userData.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserData = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
+      <span>hi</span>
+      <button>Delete Me</button>
+    </div>
+  )
+}
+
+export default UserData
--- statemangemet/src/pages/Home.jsx
+++ statemangemet/src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import UserData from "../component/userData.jsx"
 
 const Home = () => {
     return (
-        <div>Home is here</div>
+        <>
+            <div style={{ display: "flex", justifyContent: "center", gap: "50px", padding: "20px" }}>
+                <span style={{ fontWeight: "bold" }}>USER DATA</span>
+                <button>Add User</button>
+            </div>
+
+            {/* <UserData /> */}
+        </>
     )
 }
 
