9ca65a8f9da1fb9ee0668b516f828b44c37e037e
tsahil7922-prog <tsahil7922@gmail.com>
2026-09-03 15:49:01 +0000

    fake data done

--- statemangemet/src/pages/Home.jsx
+++ statemangemet/src/pages/Home.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
-import UserData from "../component/userData.jsx"
-import { useDispatch } from 'react-redux';
+import React from "react";
+import UserData from "../component/userData.jsx";
+import { useDispatch } from "react-redux";
+import { fakeData } from "../utils/fakeData.js";
 
 const Home = () => {
-    const dispatch = useDispatch()
-    const addUser=()=>{
-         
-    }
+    const dispatch = useDispatch();
+
+    const addUser = () => {
+        const user = fakeData();
+        console.log(user);
+
+        // later:
+        // dispatch(addUserAction(user));
+    };
+
     return (
         <>
-            <div style={{ display: "flex", justifyContent: "center", gap: "50px", padding: "20px" }}>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    gap: "50px",
+                    padding: "20px"
+                }}
+            >
                 <span style={{ fontWeight: "bold" }}>USER DATA</span>
-                <button onClick={()=>addUser()}>Add User</button>
+
+                <button onClick={addUser}>
+                    Add User
+                </button>
             </div>
 
-            {/* <UserData /> */}
+            <UserData />
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
--- statemangemet/src/store/slices/userSlices.js
+++ statemangemet/src/store/slices/userSlices.js
@@ -3,13 +3,17 @@ const userSlice = createSlice({
     name: "user",
     initialState: [],
     reducers: {
-        addUser: (state,action) => { },
-        removeUser: (state,action) => { },
-        clearAllUser: (state,action) => { },
+        addUser: (state, action) => { },
+        removeUser: (state, action) => { },
+        clearAllUser: (state, action) => { },
     }
 })
 
 
+// export these where u need to use 
+export const { addUser, removeUser, clearAllUser } = userSlice()
 
+// shows all the actions we made for useSlice
 console.log(userSlice.actions)
+
 export default userSlice.reducer
