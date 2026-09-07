531810cbb3a150d0aec914f3b52ad511903bc576
tsahil7922-prog <tsahil7922@gmail.com>
2026-09-07 15:04:41 +0000

    ui part done

--- statemangemet/src/component/userData.jsx
+++ statemangemet/src/component/userData.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeUser } from "../store/slices/userSlices";
 
 const UserData = () => {
-  return (
-    <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
-      <span>hi</span>
-      <button>Delete Me</button>
-    </div>
-  )
-}
+    const data = useSelector((state) => state.users);
+    const dispatch = useDispatch();
 
-export default UserData
+    console.log(data);
+
+    return (
+        <div>
+            {data.length === 0 ? (
+                <h3 style={{ textAlign: "center" }}>
+                    No User Added
+                </h3>
+            ) : (
+                data.map((user, index) => (
+                    <div
+                        key={index}
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            gap: "20px",
+                            margin: "10px"
+                        }}
+                    >
+                        <span>
+                            {index + 1}. {user}
+                        </span>
+
+                        <button
+                            onClick={() => dispatch(removeUser(index))}
+                        >
+                            Delete Me
+                        </button>
+                    </div>
+                ))
+            )}
+        </div>
+    );
+};
+
+export default UserData;
